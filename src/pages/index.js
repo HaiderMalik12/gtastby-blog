b1397bd48e7e648d6776c7a9e1477ede41aed4f6
HaiderMalik12 <haidermalik504@gmail.com>
2020-03-14 18:47:24 +0500

    fetched posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,23 @@ import { Link } from 'gatsby';
 import Layout from '../components/layout';
 
 import '../../src/styles.scss';
+import usePosts from '../hooks/use-posts';
 
-export default () => (
-  <Layout>
-    <h3>Welcome to Gatsby I am learning Gatsby</h3>
-    <p>
-      <Link to="/contact">Contact Us</Link>
-    </p>
-  </Layout>
-);
+export default () => {
+  const posts = usePosts();
+  return (
+    <Layout>
+      <h3>Welcome to Gatsby I am learning Gatsby</h3>
+      <p>
+        <Link to="/contact">Contact Us</Link>
+      </p>
+      {/* display all the blog post content here */}
+      {posts.map(p => (
+        <>
+          <h1>{p.title}</h1>
+          <p>{p.excerpt}</p>
+        </>
+      ))}
+    </Layout>
+  );
+};
