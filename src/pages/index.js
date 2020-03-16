4dd268dd6502811134656334529c56f21fcc456d
HaiderMalik12 <haidermalik504@gmail.com>
2020-03-17 02:43:21 +0500

    add images

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,22 @@ import Layout from '../components/layout';
 import '../../src/styles.scss';
 import usePosts from '../hooks/use-posts';
 import PostView from '../components/post-view';
+import { useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image'
 
 export default () => {
   const posts = usePosts();
+  const result = useStaticQuery(graphql`
+    query {
+      code: file(relativePath: { eq: "code.jpg" }) {
+        sharp: childImageSharp {
+          fluid(maxWidth: 1000, maxHeight: 300) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `);
   return (
     <Layout>
       <section className="hero">
@@ -16,6 +29,7 @@ export default () => {
             <h2 className="subtitle">
               You will learn how to find fast static websites using Gatsby
             </h2>
+          <Img fluid={result.code.sharp.fluid}/>
           </div>
         </div>
       </section>
